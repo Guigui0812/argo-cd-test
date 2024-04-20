de4444965cca67f5e44d759e0bd597c05782e183
Guigui0812 <guillaume.rohee0812@gmail.com>
2024-04-20 15:08:49 +0000

    New push to test argo

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use(express.json());
 
 // Route principale pour vérifier si le serveur fonctionne
 app.get('/', async (req, res) => {
-    res.json({ message: 'Hello World Weather !' });
+    res.json({ message: 'Hello World Weathers !' });
 });
 
 // Route pour obtenir la température d'une ville spécifique
